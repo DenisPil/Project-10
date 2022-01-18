e78cc5ae6073160d7f2897fb9dcdcf71444cc494
DenisPil <denis.pilongery@gmail.com>
2022-01-18 20:39:22 +0100

    début models

--- softdesk/api/models.py
+++ softdesk/api/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, Group
 
-# Create your models here.
+
+class User(AbstractUser):
+    """class utilisateurs"""
+
+
+class Project(models.Model):
+    title = models.fields.CharField(max_length=128)
+    description = 
+    type = models.fields.CharField(max_length=128)
+    author_user_id = models.ForeignKey(User, null=True, on_delete=models.SET_NULL, blank=True)
+
+
+class Contributor(models.Model):
+    CREATOR = 'CREATOR'
+    CONTRIBUTOR = 'CONTRIBUTOR'
+
+    ROLE_CHOICES = (
+        (CREATOR, 'Créateur'),
+        (CONTRIBUTOR, 'Contributeur'),
+    )
+    user_id = models.ForeignKey(User, null=True, on_delete=models.SET_NULL, blank=True)
+    project_id = models.ForeignKey(Project, null=True, on_delete=models.SET_NULL, blank=True)
+    role = models.CharField(max_length=30, choices=ROLE_CHOICES, verbose_name='rôle')
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        if self.role == self.CREATOR:
+            group = Group.objects.get(name='creator')
+            group.user_set.add(self)
+        elif self.role == self.SUBSCRIBER:
+            group = Group.objects.get(name='contributor')
+            group.user_set.add(self)
+
+
+
